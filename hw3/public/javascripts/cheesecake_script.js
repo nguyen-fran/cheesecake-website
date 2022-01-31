90faedf984e2041540fc574ed45160c49ef16108
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-01-30 22:20:33 -0800

    moved js script from .html file to new .js file, and made separate event handlers

---
+++ hw3/public/javascripts/cheesecake_script.js
@@ -0,0 +1,50 @@
+/**
+ * author: Francisco Nguyen
+ */
+
+//handler for when users clicks the submit button of the form
+orderSubmitHandler = function (event) {
+    var top_notes = $("#special_instr").val();
+
+    //check if the notes textbox contains the word "vegan"
+    if (top_notes.toLowerCase().includes("vegan")) {
+        alert("This cheesecake contains dairy.");
+    } else {
+        //building the submitted form text based on the user's inputs
+        var thank_you = "<p>Thank you! Your order has been placed.</p>";
+        var top_num = $("<p></p>").text("Quantity: " + $("#topping_num").val());
+
+        /*
+        help: needed to get the value of the selected radio button
+        source: https://www.geeksforgeeks.org/how-to-get-value-of-selected-radio-button-using-javascript/
+        */
+        var top_radio = document.getElementsByName("topping");
+        var top_quant;
+        //iterate through radio buttons to find which one was checked
+        for (i = 0; i < top_radio.length; i++) {
+            if (top_radio[i].checked) {
+                top_quant = "Topping: " + top_radio[i].value;
+                break;
+            }
+        }
+        top_notes = "Notes: " + top_notes;
+
+        //hiding form from page and printing thank you text
+        $("img").after(thank_you, top_quant, top_num, top_notes);
+        $("#order_form").hide();
+        event.preventDefault();
+    }
+}
+
+//handler for when user interacts with the month dropdown menu
+monthDropdownHandler = function (event) {
+    $("#month").text($(this).text());
+}
+
+$(document).ready(function(){
+    //actions for when the submit button for the form is clicked
+    $("#order_submit_button").click(orderSubmitHandler);
+
+    //changes the text for order month to the one clicked on in drop down menu
+    $("a").click(monthDropdownHandler);
+});
