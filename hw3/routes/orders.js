030a62c62127fa9ac5251fa65152f0116f2c8905
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-02-14 10:54:42 -0800

    Changing the month works once or twice

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -3,32 +3,60 @@
 var express = require('express');
 var router = express.Router();
 
-var dbms = require('./dbms');
+// var dbms = require('./dbms');
+var dbms = require('./dbms_promise');
 
 /* POST orders JSON. */
 router.post('/', function(req, res, next) {
     var month = req.body.month.toUpperCase();
     console.log("Month changed to: " + month);
 
-    var query = "select sum(QUANTITY) from ORDERS " +
+    var query = "select sum(QUANTITY) as TOPPINGS from ORDERS " +
                 "where month='" + month + "' and topping='cherry'" +
                 " union " +
                 "select sum(QUANTITY) from ORDERS " +
                 "where month='" + month + "' and topping='chocolate'" +
                 " union " +
-                "select sum(QUANTITY) from ORDERS "
+                "select sum(QUANTITY) from ORDERS " + 
                 "where month='" + month + "' and topping='plain'";
-    var month_data = dbms.dbquery(query, function(){});
     
-    res.json({
-        error: null,
-        data: [
-            {topping: "cherry", quantity: 1},
-            {topping: "chocolate", quantity: 2},
-            {topping: "plain", quantity: 3},
-        ]
-    });
+    // var month_data = dbms.dbquery(query, function(error, results) {
+    //     console.log("data: "+ results[0].TOPPINGS);
+        
+    //     var cherry = (results[0].TOPPINGS == null) ? 0 : results[0].TOPPINGS;
+    //     var chocolate = (results[1].TOPPINGS == null) ? 0 : results[1].TOPPINGS;
+    //     var plain = (results[2].TOPPINGS == null) ? 0 : results[2].TOPPINGS;
 
+    //     res.json({
+    //         error: null,
+    //         data: [
+    //             {topping: "cherry", quantity: cherry},
+    //             {topping: "chocolate", quantity: chocolate},
+    //             {topping: "plain", quantity: plain}
+    //         ]
+    //     });
+    // });
+
+    var month_data = dbms.dbquery(query);
+    // var cherry, chocolate, plain;
+    month_data.then(function(results) {
+        console.log("promise results[2].'sum(quantity)': " + results[0].topping);
+
+        var cherry = (results[0].TOPPINGS == null) ? 0 : results[0].TOPPINGS;
+        var chocolate = (results[1].TOPPINGS == null) ? 0 : results[1].TOPPINGS;
+        var plain = (results[2].TOPPINGS == null) ? 0 : results[2].TOPPINGS;
+
+        res.json({
+            error: null,
+            data: [
+                {topping: "cherry", quantity: cherry},
+                {topping: "chocolate", quantity: chocolate},
+                {topping: "plain", quantity: plain}
+            ]
+        });
+    });
+    console.log("month_data: " + month_data);
+    
 });
 
 module.exports = router;
