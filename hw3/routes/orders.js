5a91d5b39f865ceed1de1d8bf129539b130de802
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-02-13 20:03:47 -0800

    Can make sql query but can't figure out return val

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET orders JSON. */
-router.get('/', function(req, res, next) {
+var dbms = require('./dbms');
+
+/* POST orders JSON. */
+router.post('/', function(req, res, next) {
+    var month = req.body.month.toUpperCase();
+    var query = "SELECT SUM(QUANTITY) FROM ORDERS " +
+                "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'" +
+                " union " +
+                "SELECT SUM(QUANTITY) FROM ORDERS " +
+                "WHERE MONTH='" + month + "' AND TOPPINGS='chocolate'" +
+                " union " +
+                "SELECT SUM(QUANTITY) FROM ORDERS " +
+                "WHERE MONTH='" + month + "' AND TOPPINGS='plain;";
+    // var query = "SELECT SUM(QUANTITY) AS COUNT FROM ORDERS " +
+    //             "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'";
+    // var query = "select * from ORDERS where orderid=1";
+    var cherry, chocolate, plain;
+    var month_data = dbms.dbquery(query, function(error, results) {
+        cherry = results;
+        console.log("Printing cherry: " + results['MONTH']);
+    });
+
     res.json({
         error: null,
         data: [
-            {topping: "cherry", quantity: 1},
+            {topping: month_data, quantity: 1},
             {topping: "chocolate", quantity: 2},
             {topping: "plain", quantity: 3},
         ]
