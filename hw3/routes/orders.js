94a2dbc6b4592b01459861093d0ce893d610b1b9
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-02-13 20:11:14 -0800

    Back to something that hopefully works

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -5,23 +5,23 @@ var dbms = require('./dbms');
 
 /* POST orders JSON. */
 router.post('/', function(req, res, next) {
-    var month = req.body.month.toUpperCase();
-    var query = "SELECT SUM(QUANTITY) FROM ORDERS " +
-                "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'" +
-                " union " +
-                "SELECT SUM(QUANTITY) FROM ORDERS " +
-                "WHERE MONTH='" + month + "' AND TOPPINGS='chocolate'" +
-                " union " +
-                "SELECT SUM(QUANTITY) FROM ORDERS " +
-                "WHERE MONTH='" + month + "' AND TOPPINGS='plain;";
-    // var query = "SELECT SUM(QUANTITY) AS COUNT FROM ORDERS " +
-    //             "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'";
-    // var query = "select * from ORDERS where orderid=1";
-    var cherry, chocolate, plain;
-    var month_data = dbms.dbquery(query, function(error, results) {
-        cherry = results;
-        console.log("Printing cherry: " + results['MONTH']);
-    });
+    // var month = req.body.month.toUpperCase();
+    // var query = "SELECT SUM(QUANTITY) FROM ORDERS " +
+    //             "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'" +
+    //             " union " +
+    //             "SELECT SUM(QUANTITY) FROM ORDERS " +
+    //             "WHERE MONTH='" + month + "' AND TOPPINGS='chocolate'" +
+    //             " union " +
+    //             "SELECT SUM(QUANTITY) FROM ORDERS " +
+    //             "WHERE MONTH='" + month + "' AND TOPPINGS='plain;";
+    // // var query = "SELECT SUM(QUANTITY) AS COUNT FROM ORDERS " +
+    // //             "WHERE MONTH='" + month + "' AND TOPPINGS='cherry'";
+    // // var query = "select * from ORDERS where orderid=1";
+    // var cherry, chocolate, plain;
+    // var month_data = dbms.dbquery(query, function(error, results) {
+    //     cherry = results;
+    //     console.log("Printing cherry: " + results['MONTH']);
+    // });
 
     res.json({
         error: null,
