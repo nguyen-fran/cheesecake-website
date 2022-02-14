c0a07c3f12a5f17ed935c2c2f2a7718983a96796
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-02-13 19:59:49 -0800

    Revert "orders.js recognizes what month to get data for"
    This reverts commit 8f89e70728b8de05cb6157a254f808a3bed8db7a.

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -1,24 +1,8 @@
-/* author: Francisco Nguyen */
-
 var express = require('express');
 var router = express.Router();
 
-var dbms = require('./dbms');
-
-/* POST orders JSON. */
-router.post('/', function(req, res, next) {
-    var month = req.body.month.toUpperCase();
-    console.log("Month changed to: " + month);
-    var query = "select sum(QUANTITY) from ORDERS " +
-                "where month=" + month + " and topping='cherry'" +
-                " union " +
-                "select sum(QUANTITY) from ORDERS " +
-                "where month=" + month + " and topping='chocolate'" +
-                " union " +
-                "select sum(QUANTITY) from ORDERS "
-                "where month=" + month + " and topping='plain'";
-    var month_data = dbms.dbquery(query, function(){});
-    
+/* GET orders JSON. */
+router.get('/', function(req, res, next) {
     res.json({
         error: null,
         data: [
@@ -27,7 +11,6 @@ router.post('/', function(req, res, next) {
             {topping: "plain", quantity: 3},
         ]
     });
-
 });
 
 module.exports = router;
